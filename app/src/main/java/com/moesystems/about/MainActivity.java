0714593c48ef5ed8f471f6462309cc18b913810a
Moemeister <00062716@uca.edu.sv>
2018-04-06 23:37:12 -0600

    Added Share button

--- app/src/main/java/com/moesystems/about/MainActivity.java
+++ app/src/main/java/com/moesystems/about/MainActivity.java
@@ -3,6 +3,7 @@ package com.moesystems.about;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -17,12 +18,60 @@ import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //Button sendBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
 
+        sendBtn = (Button)findViewById(R.id.btn_share);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri pictureUri = Uri.parse("android:resource://com.moesystems.about/drawable/"+R.drawable.yo);
+
+                Intent sendIntent =  new Intent();
+
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("image/*");
+                sendIntent.putExtra(Intent.EXTRA_STREAM, pictureUri);
+
+                sendIntent.putExtra(Intent.EXTRA_TEXT,((TextView)findViewById(R.id.name)).getText().toString() +" "+
+                        ((TextView)findViewById(R.id.fb_name)).getText().toString() +" "+
+                        ((TextView)findViewById(R.id.git_user)).getText().toString() +" "+
+                        ((TextView)findViewById(R.id.phone)).getText().toString() +" " +
+                        ((TextView)findViewById(R.id.mail)).getText().toString());
+
+
+                if (sendIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(sendIntent);
+                }
+
+
+            }
+        });*/
+
+
+    }
+    public void share(View v){
+        Uri pictureUri = Uri.parse("android.resource://" + getPackageName() + "/drawable/" + R.drawable.yo);
+
+        Intent sendIntent =  new Intent();
+
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setType("image/*");
+        sendIntent.putExtra(Intent.EXTRA_STREAM, pictureUri);
+
+        sendIntent.putExtra(Intent.EXTRA_TEXT,"Name: "+((TextView)findViewById(R.id.name)).getText().toString()+"\nFacebook: "+
+                ((TextView)findViewById(R.id.fb_name)).getText().toString() +"\nGithub: "+
+                ((TextView)findViewById(R.id.git_user)).getText().toString() +"\nPhone: "+
+                ((TextView)findViewById(R.id.phone)).getText().toString() +"\nE-mail: " +
+                ((TextView)findViewById(R.id.mail)).getText().toString());
+
+        if(sendIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(sendIntent);
+        }
 
 
     }
